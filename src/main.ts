58d41d9daeaa83cc3da81eae1915fa278ead7c55
Codebolt <dathphan@ucsc.edu>
2024-10-15 14:44:08 -0700

    Step 10 Complete

--- src/main.ts
+++ src/main.ts
@@ -32,14 +32,16 @@ interface Item {
   cost: number;
   upgrade: number;
   name: string;
+  description: string;
   button: HTMLButtonElement;
   count: number;
-};
+}
 
 const tier1: Item = {
   cost: 10,
   upgrade: 0.1,
   name: "Rub",
+  description: "relaxing :)",
   button: document.createElement("button"),
   count: 0,
 };
@@ -48,32 +50,59 @@ const tier2: Item = {
   cost: 100,
   upgrade: 2.0,
   name: "Brush",
+  description: "calm and soothing :D",
   button: document.createElement("button"),
   count: 0,
 };
 
 const tier3: Item = {
-  cost: 1000,
-  upgrade: 50,
+  cost: 350,
+  upgrade: 15,
   name: "Snack",
+  description: "homemade. delicious, tasty, juicy :3",
   button: document.createElement("button"),
   count: 0,
 };
 
-const upgrades: Item[] = [tier1, tier2, tier3];
+const tier4: Item = {
+  cost: 1000,
+  upgrade: 50,
+  name: "Pillow",
+  description: "very soff and fluffy :3c",
+  button: document.createElement("button"),
+  count: 0,
+};
+
+const tier5: Item = {
+  cost: 9000,
+  upgrade: 9000,
+  name: "Purr Master 9000",
+  description: "The true endgame for any feline. >:3",
+  button: document.createElement("button"),
+  count: 0,
+};
+
+const upgrades: Item[] = [tier1, tier2, tier3, tier4, tier5];
+
+let description = document.createElement("div");
+app.append(description);
 
 upgrades.forEach((u) => {
-  u.button.innerHTML =
-    u.name + " [" + parseFloat(u.cost.toFixed(3)).toString() + "]";
-  u.button.addEventListener("click", () => {
-    upgradeAutoclick(u.upgrade, u.cost);
-    u.count++;
-    u.cost *= inflationRate;
-    u.button.innerHTML =
-      u.name + " [" + parseFloat(u.cost.toFixed(3)).toString() + "]";
-  });
-  app.append(u.button);
+    u.button.innerHTML = u.name + " [" + parseFloat(u.cost.toFixed(3)).toString() + "]";
+    u.button.addEventListener("click", () => {
+      upgradeAutoclick(u.upgrade, u.cost);
+      u.count++;
+      u.cost *= inflationRate;
+    });
+    u.button.addEventListener("mouseenter", () => {
+      description.innerHTML = u.description;
+    });
+    u.button.addEventListener("mouseleave", () => {
+      description.innerHTML = "...";
+    });
+    app.append(u.button);
 });
+app.append(description);
 
 const upgradeStatus = document.createElement("div");
 updateStatus();
@@ -113,23 +142,19 @@ function autoClick(timestep: number) {
 
 function updateCounter() {
   counterDisplay.innerHTML =
-    "<h2>Purr Frequency: </h2><h1>" +
-    parseFloat(counter.toFixed(3)).toString() +
-    "</h1>";
+    "<h2>Purr Frequency: </h2><h1>" + counter.toFixed(3) + "</h1>";
 }
 
 function updateButtons() {
   upgrades.forEach((u) => {
     u.button.disabled = counter < u.cost;
+    u.button.innerHTML = u.name + " [" + parseFloat(u.cost.toFixed(3)).toString() + "]";
   });
 }
 
 function updateStatus() {
   let text: string = "";
-  text +=
-    "<h3>" +
-    parseFloat(autoClickRate.toFixed(1)).toString() +
-    " tigers/sec</h3>";
+  text += "<h3>" + parseFloat(autoClickRate.toFixed(1)).toString() + " tigers/sec</h3>";
   upgrades.forEach((u) => {
     text += u.count + " " + u.name + (u.count != 1 ? "s<br>" : "<br>");
   });
